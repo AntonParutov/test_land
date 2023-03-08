7051c7345c7dc1852dfbdb987b99b6c6438d11c4
AntonParutov <115708359+AntonParutov@users.noreply.github.com>
2023-03-08 11:41:56 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 let menuBtn = document.querySelector(".menu-btn");
 let menu = document.querySelector(".menu");
-let link = document.querySelector("a.header_nav__item");
+let link = document.querySelector(".header_nav__item");
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 });
-link.addEventListener("click", function () {
+
+menu.onclick = function () {
   menu.classList.remove("active");
   menuBtn.classList.remove("active");
-});
+};
